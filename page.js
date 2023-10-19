27c303c5c3a709c71a13c80ac6d86b7138f24981
saraggarde <148424572+saraggarde@users.noreply.github.com>
2023-10-19 08:46:32 +0200

    Update page.js

--- page.js
+++ page.js
@@ -1,21 +1,8 @@
-// Este es un comentario de una sola línea
-
-/* Este es un comentario
-   de varias líneas */
-
 // Declaración de una variable
 var mensaje = "¡Hola, mundo!";
 
 // Muestra el mensaje en la consola del navegador
 console.log(mensaje);
-
-// Función que muestra una alerta en la página web
-function mostrarAlerta() {
-    alert(mensaje);
-}
-
-// Llamada a la función cuando se hace clic en un botón
-document.getElementById("boton").addEventListener("click", mostrarAlerta);
 
 webgazer.begin()
 webgazer.setGazeListener(function(data, elapsedTime) {
@@ -24,7 +11,5 @@ webgazer.setGazeListener(function(data, elapsedTime) {
 	}
 	var xprediction = data.x; //these x coordinates are relative to the viewport
 	var yprediction = data.y; //these y coordinates are relative to the viewport
-   console.log(data.x)
-   console.log(data.y)
-	console.log(elapsedTime); //elapsed time is based on time since begin was called
+   console.log(data.x, data.y)
 }).begin();

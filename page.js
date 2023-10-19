fcd20c7b03e8e8045188a1b60b4484ed33cd8873
saraggarde <148424572+saraggarde@users.noreply.github.com>
2023-10-19 13:10:53 +0200

    Update page.js

--- page.js
+++ page.js
@@ -1,14 +1,57 @@
 console.log("Ancho de pantalla: " + screen.width);
 console.log("Alto de pantalla: " + screen.height);
+var estado = 0;
+var LeftTop = document.getElementById("LeftTop");
+var LeftMid = document.getElementById("LeftMid");
+var LeftBot = document.getElementById("LeftBot");
+
+var MidTop = document.getElementById("MidTop");
+var MidMid = document.getElementById("MidMid");
+var MidBot = document.getElementById("MidBot");
+
+var RigTop = document.getElementById("RigTop");
+var RigMid = document.getElementById("RigMid");
+var RigBot = document.getElementById("RigBot");
+
+LeftTop.style.display = "none";
+LeftMid.style.display = "none";
+LeftBot.style.display = "none";
+MidTop.style.display = "none";
+MidMid.style.display = "none";
+MidBot.style.display = "none";
+RigTop.style.display = "none";
+RigMid.style.display = "none";
+RigBot.style.display = "none";
 
 
-document.getElementById('LeftMid').addEventListener('click', function() {
-  var boton = document.getElementById("LeftMid");
-  console.log("LeftMid: " + boton.offsetLeft + ", "+ boton.offsetTop);
+LeftTop.addEventListener('click', function() {
+  console.log("LeftTop: " + LeftTop.offsetLeft + ", "+ LeftTop.offsetTop);
 });
-document.getElementById('LeftBot').addEventListener('click', function() {
-  var boton = document.getElementById("LeftBot");
-  console.log("LeftBot: " + boton.offsetLeft + ", "+ boton.offsetTop);
+LeftMid.addEventListener('click', function() {
+  console.log("LeftMid: " + LeftMid.offsetLeft + ", "+ LeftMid.offsetTop);
+});
+LeftBot.addEventListener('click', function() {
+  console.log("LeftBot: " + LeftBot.offsetLeft + ", "+ LeftBot.offsetTop);
+});
+
+MidTop.addEventListener('click', function() {
+  console.log("MidTop: " + MidTop.offsetLeft + ", "+ MidTop.offsetTop);
+});
+MidMid.addEventListener('click', function() {
+  console.log("MidMid: " + MidMid.offsetLeft + ", "+ MidMid.offsetTop);
+});
+MidBot.addEventListener('click', function() {
+  console.log("MidBot: " + MidBot.offsetLeft + ", "+ MidBot.offsetTop);
+});
+
+RigTop.addEventListener('click', function() {
+  console.log("RigTop: " + RigTop.offsetLeft + ", "+ RigTop.offsetTop);
+});
+RigMid.addEventListener('click', function() {
+  console.log("RigMid: " + RigMid.offsetLeft + ", "+ RigMid.offsetTop);
+});
+RigBot.addEventListener('click', function() {
+  console.log("RigBot: " + RigBot.offsetLeft + ", "+ RigBot.offsetTop);
 });
 
 webgazer.begin()

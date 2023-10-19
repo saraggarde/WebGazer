00d302910fe8b42a47b5a061cfde483a12bd8128
saraggarde <148424572+saraggarde@users.noreply.github.com>
2023-10-19 08:23:23 +0200

    Update page.js

--- page.js
+++ page.js
@@ -18,3 +18,13 @@ function mostrarAlerta() {
 document.getElementById("boton").addEventListener("click", mostrarAlerta);
 
 webgazer.begin()
+webgazer.setGazeListener(function(data, elapsedTime) {
+	if (data == null) {
+		return;
+	}
+	var xprediction = data.x; //these x coordinates are relative to the viewport
+	var yprediction = data.y; //these y coordinates are relative to the viewport
+   console.log(data.x)
+   console.log(data.y)
+	console.log(elapsedTime); //elapsed time is based on time since begin was called
+}).begin();

d5e57f64a83ff06ac241c6e20cf228ad6bff9d08
saraggarde <148424572+saraggarde@users.noreply.github.com>
2023-10-23 07:55:30 +0200

    Update page.js

--- page.js
+++ page.js
@@ -113,24 +113,88 @@ RigBot.addEventListener('click', function() {
 	if (clicks == 5) {
 		clicks = 0;
 		RigBot.style.display = "none";
-		if (estado == "cal") MidMid.style.display = "block";
-		if (estado == "val") {
-			var blob = new Blob([contenido], { type: 'text/plain' });
-			var enlace = document.createElement('a');
-			enlace.href = URL.createObjectURL(blob);
-			enlace.download = 'archivo.txt';
-			enlace.innerHTML = 'Descargar archivo';
-			enlace.style.position = 'fixed';
-			enlace.style.top = '50%';
-			enlace.style.left = '50%';
-			document.body.appendChild(enlace);
-			webgazer.pause();
-			webgazer.resume();
-			console.log("Fin");
-		}
+		MidMid.style.display = "block";
 		estado = "val";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_MidMid: " + MidMid.offsetLeft + ", "+ MidMid.offsetTop + "\n");
 	}
 });
+
+function mostrarSiguiente() {
+	// 1
+	if (MidMid.style.display == "block") {
+		MidMid.style.display = "none";
+		LeftTop.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_LeftTop: " + LeftTop.offsetLeft + ", "+ LeftTop.offsetTop + "\n");
+	}
+	// 2
+	if (LeftTop.style.display == "block") {
+		LeftTop.style.display = "none";
+		MidTop.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_MidTop: " + MidTop.offsetLeft + ", "+ MidTop.offsetTop + "\n");
+	}
+	// 3
+	if (MidTop.style.display == "block") {
+		MidTop.style.display = "none";
+		RigTop.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_RigTop: " + RigTop.offsetLeft + ", "+ RigTop.offsetTop + "\n");
+	}
+	// 4
+	if (RigTop.style.display == "block") {
+		RigTop.style.display = "none";
+		LeftMid.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_LeftMid: " + LeftMid.offsetLeft + ", "+ LeftMid.offsetTop + "\n");
+	}
+	// 5
+	if (LeftMid.style.display == "block") {
+		LeftMid.style.display = "none";
+		RigMid.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_RigMid: " + RigMid.offsetLeft + ", "+ RigMid.offsetTop + "\n");
+	}
+	// 6
+	if (RigMid.style.display == "block") {
+		RigMid.style.display = "none";
+		LeftBot.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_LeftBot: " + LeftBot.offsetLeft + ", "+ LeftBot.offsetTop + "\n");
+	}
+	// 7
+	if (LeftBot.style.display == "block") {
+		LeftBot.style.display = "none";
+		MidBot.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_MidBot: " + MidBot.offsetLeft + ", "+ MidBot.offsetTop + "\n");
+	}
+	// 8
+	if (MidBot.style.display == "block") {
+		MidBot.style.display = "none";
+		RigBot.style.display = "block";
+		setTimeout(mostrarSiguiente, 5000);
+		contenido += ("Val_RigBot: " + RigBot.offsetLeft + ", "+ RigBot.offsetTop + "\n");
+	}
+	// 9
+	if (RigBot.style.display == "block") {
+		RigBot.style.display = "none";
+		var blob = new Blob([contenido], { type: 'text/plain' });
+		var enlace = document.createElement('a');
+		enlace.href = URL.createObjectURL(blob);
+		enlace.download = 'archivo.txt';
+		enlace.innerHTML = 'Descargar archivo';
+		enlace.style.position = 'fixed';
+		enlace.style.top = '50%';
+		enlace.style.left = '50%';
+		document.body.appendChild(enlace);
+		webgazer.pause();
+		webgazer.resume();
+		console.log("Fin");
+	}
+	
+}
 
 webgazer.begin()
 webgazer.setGazeListener(function(data, elapsedTime) {
